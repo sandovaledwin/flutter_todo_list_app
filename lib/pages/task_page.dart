3a639453da1930cdcea9481fe10eb7570b00ed5c
Edwin Sandoval <edwin.sandoval@peopleconnect.us>
2020-05-18 09:59:18 -0500

    Added the add task and add event pages

---
+++ lib/pages/task_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TaskPage extends StatefulWidget {
+  @override
+  _TaskPageState createState() => _TaskPageState();
+}
+
+class _TaskPageState extends State<TaskPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center();
+  }
+
+}
